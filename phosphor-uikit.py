64b14b6881f101c35a546a8470b96e0785d4cbae
Jason Pepas <jasonpepas@gmail.com>
2024-10-09 20:59:59 -0500

    wip

--- phosphor-uikit.py
+++ phosphor-uikit.py
@@ -7,8 +7,13 @@
 import os
 import sys
 import json
+import shutil
 
 # Globals:
+
+flags = {
+    "--dry-run": False
+}
 
 valid_styles = set(["bold", "duotone", "fill", "light", "regular", "thin"])
 
@@ -24,7 +29,7 @@ valid_renderers = ["rsvg", "inkscape"]
 
 # Load a JSON config file.
 def load_config_fname(config_fname):
-    if config_fname[-5:] != ".json":
+    if not config_fname.endswith(".json"):
         sys.stderr.write("❌ Error: config file does not have suffix '.json': %s\n" % config_fname)
         sys.exit(1)
     # Try to open the file.
@@ -69,16 +74,18 @@ def parse_options(config, config_fname):
 
 
 # Parse out all of the icon groups from the config object.
-# Returns a dict of icon name to set of style-size tuples:
-# { "person": { (regular,44), (bold,44) } }
+# Returns a set of (icon name, size, style) triples.:
+# { ("person",44,"regular"), ("person",44,"bold") }
 def parse_icon_groups(config, config_fname):
-    icon_groups_dict = {}
+    icons_set = set()
     for group in config:
         if type(group) is not list:
+            # This is either a comment or a config option.  Skip.
             continue
+        # Collect the names, sizes and styles in this group.
         group_icon_names = set()
-        group_styles = set()
         group_sizes = set()
+        group_styles = set()
         for word in group:
             if type(word) is int:
                 # This is a size.
@@ -91,57 +98,146 @@ def parse_icon_groups(config, config_fname):
                     # This is an icon name.
                     group_icon_names.add(word)
             else:
+                # Unrecognized junk in this group.
                 sys.stderr.write("❌ Error: %s: unexpected value '%s'.\n" % (config_fname, word))
                 sys.exit(1)
-        if len(group_styles) == 0:
-            group_styles = default_styles.copy()
+        # If we didn't find any sizes or styles, use the defaults.
         if len(group_sizes) == 0:
             group_sizes = default_sizes.copy()
-        # Make a set of all of the (style, size) pairs:
-        style_size_set = set()
-        for style in group_styles:
+        if len(group_styles) == 0:
+            group_styles = default_styles.copy()
+        # Do the combinatorial explosion and add them to the set.
+        for name in group_icon_names:
             for size in group_sizes:
-                t = (style, size)
-                style_size_set.add(t)
-        # Union the set with any existint set:
-        for icon_name in group_icon_names:
-            if icon_name in icon_groups_dict:
-                style_size_set = style_size_set.union(icon_groups_dict[icon_name])
-            icon_groups_dict[icon_name] = style_size_set
-    return icon_groups_dict
+                for style in group_styles:
+                    triple = (name, size, style)
+                    icons_set.add(triple)
+    return icons_set
+
+
+# Generate a .imageset Contents.json file.
+# An example:
+# {
+#   "images" : [
+#     {
+#       "filename" : "user.regular.44.png",
+#       "idiom" : "universal",
+#       "scale" : "1x"
+#     },
+#     {
+#       "filename" : "user.regular.44@2x.png",
+#       "idiom" : "universal",
+#       "scale" : "2x"
+#     },
+#     {
+#       "filename" : "user.regular.44@3x.png",
+#       "idiom" : "universal",
+#       "scale" : "3x"
+#     }
+#   ],
+#   "info" : {
+#     "author" : "xcode",
+#     "version" : 1
+#   }
+# }
+def make_imageset_contents_json():
+    pass
+
+
+# Generate a .xcassets Contents.json file.
+# An example:
+# {
+#   "info" : {
+#     "author" : "xcode",
+#     "version" : 1
+#   }
+# }
+def make_xcasset_contents_json(catalog_dname):
+    # Ensure the top-level Contents.json exists.
+    fpath = catalog_dname + "/Contents.json"
+    if not os.path.exists(fpath):
+        d = {"info": {"author": "xcode", "version": 1}}
+        j = json.dumps(d, sort_keys=True, indent=4, separators=(',', ': ')) + "\n"
+        sys.stdout.write("⚙️  Creating 📄 %s\n" % fpath)
+        if not flags["--dry-run"]:
+            with open(fpath, "w+") as fd:
+                fd.write(j)
 
 
 # Generate or update an asset catalog.
-def update_asset_catalog(catalog_dname, icon_groups_dict):
-    # Ensure the .xcassets director exists.
+def update_asset_catalog(catalog_dname, icons_set):
+    # Ensure the .xcassets directory exists.
     if not os.path.exists(catalog_dname):
         os.mkdir(catalog_dname)
     # Ensure the top-level Contents.json exists.
-    contents_fname = catalog_dname + "/Contents.json"
-    if not os.path.exists(contents_fname):
-        d = {"info": {"author": "xcode", "version": 1}}
-        j = json.dumps(d, sort_keys=True, indent=4, separators=(',', ': ')) + "\n"
-        with open(contents_fname, "w+") as fd:
-            fd.write(j)
-    # generate a set of all of the filenames which should be present.
-    expected_filenames = set()
-    for icon_name, style_size_tuples in icon_groups_dict.items():
-        for (style, size) in style_size_tuples:
-            fname1 = "%s.%s.%s"
+    make_xcasset_contents_json(catalog_dname)
+    # First, reconcile the .imageset directories.
+    existing_imageset_dnames = set()
+    for dname in os.listdir(catalog_dname):
+        dpath = os.path.join(catalog_dname, dname)
+        if not os.path.isdir(dpath):
+            continue
+        if not dname.endswith(".imageset"):
+            continue
+        existing_imageset_dnames.add(dname)
+    expected_imageset_dnames = set()
+    for (name, size, style) in icons_set:
+        imageset_dname = "%s.%s.%s.imageset" % (name, size, style)
+        expected_imageset_dnames.add(imageset_dname)
+    imagesets_to_create = expected_imageset_dnames.difference(existing_imageset_dnames)
+    imagesets_to_delete = existing_imageset_dnames.difference(expected_imageset_dnames)
+    for dname in imagesets_to_delete:
+        dpath = os.path.join(catalog_dname, dname)
+        sys.stdout.write("♻️  Deleting 📁 %s\n" % dpath)
+        if not flags["--dry-run"]:
+            shutil.rmtree(dpath)
+    for dname in imagesets_to_create:
+        dpath = os.path.join(catalog_dname, dname)
+        sys.stdout.write("⚙️  Creating 📁 %s\n" % dpath)
+        if not flags["--dry-run"]:
+            os.mkdir(dpath)
+    # Now reconcile the png's within each imageset.
+    for (name, size, style) in icons_set:
+        imageset_dname = "%s.%s.%s.imageset" % (name, size, style)
+        png_fname = "%s.%s.%s.png" % (name, size, style)
 
 
 # Load and process a JSON config file.
 def process_config_fname(config_fname):
     config = load_config_fname(config_fname)
     options = parse_options(config, config_fname)
-    icon_groups_dict = parse_icon_groups(config, config_fname)
-    catalog_dname = config_fname[:-5] + ".xcassets"
-    update_asset_catalog(catalog_dname, icon_groups_dict)
+    icons_set = parse_icon_groups(config, config_fname)
+    catalog_dname = config_fname[:-(len(".json"))] + ".xcassets"
+    update_asset_catalog(catalog_dname, icons_set)
+
+
+def usage(fd):
+    if fd != sys.stderr:
+        fd.write("phosphor-uikit.py: generate PNG-based asset catalogs.\n")
+    fd.write("Usage:\n")
+    fd.write("  phosphor-uikit.py [--dry-run] config1.json config2.json ...\n")
 
 
 if __name__ == "__main__":
-    # Treat the command line arguments as a json config files.
-    config_fnames = sys.argv[1:]
+    # Parse the command line flags.
+    config_fnames = []
+    for word in sys.argv[1:]:
+        if word == "--dry-run":
+            flags["--dry-run"] = True
+        elif word == "--help":
+            usage(sys.stdout)
+            sys.exit(0)
+        elif word.endswith(".json"):
+            config_fnames += [word]
+        else:
+            sys.stderr.write("❌ Error: unrecognized argument '%s'.\n" % word)
+            usage(sys.stderr)
+            sys.exit(1)
+
+    if len(config_fnames) == 0:
+        sys.stderr.write("❌ Error: no config file arguments given.\n")
+        usage(sys.stderr)
+        sys.exit(1)
 
     for config_fname in config_fnames:
         process_config_fname(config_fname)
